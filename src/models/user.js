a54255b7859c83bf5581271c3bd6e2e200dcb866
Soner Zorlu <sonerzorlu@gmail.com>
2023-12-15 19:31:19 +0300

    user

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "admin",
@@ -25,46 +25,70 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 // User Model:
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    password:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: false,
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: false,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    firstName:{
-        type: String,
-        required: true,
-        trim: false,
+    firstName: {
+      type: String,
+      required: true,
+      trim: false,
     },
-    lastName:{
-        type: String,
-        required: true,
-        trim: false,
+    lastName: {
+      type: String,
+      required: true,
+      trim: false,
     },
-    isActive:{
-        type: String,
-        required: true,
-        trim: true,
+    isActive: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    isAdmin:{
-        type: String,
-        required: true,
-        trim: true,
+    isAdmin: {
+      type: String,
+      required: true,
+      trim: true,
     },
+  },
+  { collection: "users", timestamps: true }
+);
 
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
+UserSchema.pre("save", function (next) {
+  const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+    data.email
+  );
 
-},{collection : 'users', timestamps: true});
+  if (isEmailValidated) {
+    this.email = emailEncrypted(this.email);
+  }
+
+  const isPasswordValidated = Regexp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&].{8,}$"
+  ).test(this.password);
+  if (isPasswordValidated) {
+    this.password = passwordEncrypt(this.password);
+    next();
+  }else{
+    next(new Error("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."));
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 // routes/user:
+const user = require("../controllers/user");
+
+//URL : /users
+router.route("/")
+    .get(user.list)
+    .post(user.create);
+
+router.route("/:id")
+  .get("/", user.list)
+  .post("/", user.create)
+  .get("/:id", user.read)
+  .put("/:id", user.update)
+  .delete("/:id", user.delete);
